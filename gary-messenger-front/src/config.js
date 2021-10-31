ce50169808992c29cad61ab6496b9c64d0b1bd29
Gary <williamsg12>
2021-10-31 15:40:01 -0400

    added stuff

--- gary-messenger-front/src/config.js
+++ gary-messenger-front/src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
 	window.location.hostname === 'localhost'
-		? 'http://localhost:8000'
-		: 'https://howling-nightmare-76061.herokuapp.com/';
+		? 'https://localhost:8000'
+		: 'https://grim-spider-92723.herokuapp.com/messenger/';
 
 export default API_URL;
